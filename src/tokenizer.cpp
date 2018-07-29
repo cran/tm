bd2c9e872e5d467e58ccb24cb7cb837b5285e0f4
Ingo Feinerer <feinerer@logic.at>
2018-07-29 15:56:27 +0000

    version 0.7-5

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -8,8 +8,14 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 StringVector Boost_Tokenizer(const StringVector strings) {
     std::vector<std::string> tokens;
+    std::vector<unsigned int> places;
 
     for (unsigned int index = 0; index < strings.size(); index++) {
+	if(StringVector::is_na(strings[index])) {
+	    places.push_back(tokens.size());
+	    tokens.push_back("");
+	    continue;
+	}
         std::string str = std::string(strings(index));
 	typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
 	boost::char_separator<char> sep(" \f\n\r\t\v");
@@ -21,5 +27,10 @@ StringVector Boost_Tokenizer(const StringVector strings) {
 	}
     }
 
-    return wrap(tokens);
+    StringVector y = wrap(tokens);
+    for(unsigned int i = 0; i < places.size(); i++) {
+	y[places[i]] = NA_STRING;
+    }
+    
+    return y;
 }
